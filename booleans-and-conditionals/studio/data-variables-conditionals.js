f46c939a36d0a6e4b0bf08f4d19e7e3496cf8a08
unknown <terrell.robinson012498@gmail.comgit config --global user.name Trellyrell>
2024-03-28 14:18:16 -0500

    added node modules

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,15 +1,49 @@
 // Initialize Variables below
+let date = "Monday 2019-03-18";
+let time = "10:05:34";
+let astronautCount = 7;
+let astronauntStatus = "ready";
+let averageAstronautMassKG = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKG;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+const maximumMassLimit = 850000;
+let fuelTempCelsius = -225;
+let minimumFuelTemp = -300;
+let maximumFuelTemp = -150;
+let fuelTemp = "100%";
+let weatherStatus = "clear";
+let preparedForLiftOff = true;
+
 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
-
+if (astronautCount > 7) {
+    console.log("Too many astronaunts");
+    preparedForLiftOff = false;
+} else {
+       console.log("the perfect amount");
+}
 // add logic below to verify all astronauts are ready
-
+if (astronauntStatus != "ready") {
+    console.log("not ready");
+    preparedForLiftOff = false;
+} else {
+       console.log("ready for lift off");
+}
 // add logic below to verify the total mass does not exceed the maximum limit of 850000
 
-// add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
+if  (totalMassKg < maximumMassLimit) {
+    console.log("weight is good ");
+} else
+       console.log("weight exceeded");
+       preparedForLiftOff = false;
 
+// add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
+if (fue)
 // add logic below to verify the fuel level is at 100%
 
 // add logic below to verify the weather status is clear
 
 // Verify shuttle launch can proceed based on above conditions
+ 
--- stringing-characters-together/code-snippets/bracket-notation.js
+++ stringing-characters-together/code-snippets/bracket-notation.js
@@ -1,4 +1,57 @@
+
+// let jsCreator = "Brendan Eich";
+// let firstInitial = jsCreator[0];
+// let lastInitial = jsCreator[8];
+
+// let outputStr = "JavaScript was created by somebody with initials " + 
+//    firstInitial + "." +
+//    lastInitial + ".";
+
+// console.log(outputStr);
 let jsCreator = "Brendan Eich";
 
 console.log(jsCreator[-1]);
 console.log(jsCreator[42]);
+// let phrase = 'Code for fun';
+// console.log(phrase[2]);
+// let phrase = "JavaScript rocks!";
+// console.log(phrase[phrase.length - 8]);
+// let firstName = "Grace";
+// let lastName = "Hopper";
+
+// console.log(firstName, "has", firstName.length, "characters");
+// console.log(lastName, "has", lastName.length, "characters");
+
+// let word = 'Rutabaga';
+// word = 'Rutabaga';
+// console.log(word.length);
+
+// cityName = "Vienna";
+// stateName = "Virginia";
+// location = cityName + ", " +  stateName;
+
+// console.log(location.length);
+
+// let nonprofit = "Launchcode";
+
+// console.log(nonprofit);
+// nonprofit[6] = "C";
+// console.log(nonprofit);
+
+// pet = 'cat'
+// console.log(pet + 's');
+// pet += 's';
+
+
+let language = "JavaScript";
+language.replace('J', 'Q');
+language.slice(0,5);
+console.log(language);
+
+language = 'JavaScript';
+console.log(language.slice(1,6));
+
+let org = "  The LaunchCode Foundation ";
+let trimmed = org.trim();
+
+console.log(trimmed);
