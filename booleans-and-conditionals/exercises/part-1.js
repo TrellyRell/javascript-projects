d5e058b6bec7464cd518e9cbc02ce7450e210c35
unknown <terrell.robinson012498@gmail.comgit config --global user.name Trellyrell>
2024-04-01 09:18:28 -0500

    updating

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,11 +1,30 @@
 // Declare and initialize the variables for exercise 1 here:
-
+let engineIndicatorLight = "red blinking"
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
+let computerStatusCode = 400;
+let shuttleSpeed = 15000;
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 
-if (engineIndicatorLight === "green") {
-   console.log("engines have started");
-} else if (engineIndicatorLight === "green blinking") {
-   console.log("engines are preparing to start");
+// if (engineIndicatorLight === "green") {
+//    console.log("engines have started");
+// } else if (engineIndicatorLight === "green blinking") {
+//    console.log("engines are preparing to start");
+// } else {
+//    console.log("engines are off");
+// }
+
+if (crewStatus) {
+   console.log("crew ready");
 } else {
-   console.log("engines are off");
+    console.log("crew not ready");
+}
+
+if (computerStatusCode === 200) {
+   console.log("Please stand by. Computer is rebooting.");
+} else if (computerStatusCode === 400) {
+   console.log("Success! Computer online");
+} else {
+   console.log("ALERT: Computer offline!");
 }
