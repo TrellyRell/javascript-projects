ab25b79ae718712e11e910b7968a3836ba1f7a54
unknown <terrell.robinson012498@gmail.comgit config --global user.name Trellyrell>
2024-04-02 14:58:01 -0500

    exercise functions

--- functions/studio/studio-functions.js
+++ functions/studio/studio-functions.js
@@ -1,16 +1,22 @@
 //We want to COMPLETELY reverse an array by flipping the order of the entries AND flipping the order of characters in each element.
 
 // Part One: Reverse Characters
-
+function reverseCharacters (str) {
+    if(typeof str === "number"){
+        let newString = string(str);
+        return Number(newString.split("").reverse().join(''));
+    }
+    return str.split("").reverse().join('')
+}
+let myVariableName = 1234567;
 // 1. Define the function as reverseCharacters. Give it one parameter, which will be the string to reverse.
 // 2. Within the function, split the string into an array, then reverse the array.
 // 3. Use join to create the reversed string and return that string from the function.
 // 4. Below the function, define and initialize a variable to hold a string.
 // 5. Use console.log(reverseCharacters(myVariableName)); to call the function and verify that it correctly reverses the characters in the string.
 // 6. Optional: Use method chaining to reduce the lines of code within the function.
-
+console.log(reverseCharacters(myVariableName));
 // Part Two: Reverse Digits
-
 // 1. Add an if statement to reverseCharacters to check the typeof the parameter.
 // 2. If typeof is ‘string’, return the reversed string as before.
 // 3. If typeof is ’number’, convert the parameter to a string, reverse the characters, then convert it back into a number.
@@ -30,6 +36,12 @@ let arrayTest1 = ['apple', 'potato', 'Capitalized Words'];
 let arrayTest2 = [123, 8897, 42, 1168, 8675309];
 let arrayTest3 = ['hello', 'world', 123, 'orange'];
 
+function completeReversal(oldArr){
+    let arr = [];
+for(let i = 0; 0 < oldArr; i++) {
+    arr.unshift(reverseCharacters(oldArr[i]))
+}
+}
 // Bonus Missions
 
 // 1. Have a clear, descriptive name like funPhrase.
