fe1a31ccdd0d9c5ed13dec775b0e707a9a4b8333
unknown <terrell.robinson012498@gmail.comgit config --global user.name Trellyrell>
2024-06-12 23:42:35 -0500

    examples

--- data-and-variables/chapter-examples/bruces-beard.js
+++ data-and-variables/chapter-examples/bruces-beard.js
@@ -1,1 +1,46 @@
-console.log('Bruce's beard');
+// console.log('Bruce +  beard');
+// console.log( Number('3 3'));
+// console.log( Number('33')); 
+// console.log(typeof 'three');
+// console.log(typeof "17");
+// console.log(typeof "3.2");
+// console.log(typeof String(123));
+
+// let message = "What's up, Doc?";
+// let n = 17;
+// let pi = 3.14159;
+
+// console.log(typeof message);
+// console.log(typeof n);
+// console.log(typeof pi);
+
+// let summer = "Cash money"
+// console.log("I have " + summer + " all the time!!!!!");
+// // separate
+// let day = "Thursday";
+// console.log(day);
+
+// day = "Friday";
+// console.log(day);
+
+// day = 21;
+// console.log(day);
+
+// let Robinson = "Terrell and Summer "
+// console.log(typeof Robinson);
+
+// const appName = "Get It Done";
+// appName = "Best TODO application Ever!";
+
+// console.log(2 + 3);
+// console.log(2 - 3);
+// console.log(2 * 3);
+// console.log(2 ** 3);
+// console.log(3 ** 2);
+// console.log(1 + 5 % 3);
+
+const input = require('readline-sync');
+
+let name = input.question("Enter your name: ");
+
+console.log(typeof name);
