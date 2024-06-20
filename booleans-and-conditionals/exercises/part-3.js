57f08fc8ae19a75ff8b6586721b73a37e0cb522b
unknown <terrell.robinson012498@gmail.comgit config --global user.name Trellyrell>
2024-06-20 01:31:34 -0500

    finish later 2

--- booleans-and-conditionals/exercises/part-3.js
+++ booleans-and-conditionals/exercises/part-3.js
@@ -22,3 +22,11 @@ f) Otherwise, print "Fuel and engine status pending..." */
 
 /* 6) b) Code the following if/else check:
 If fuelLevel is above 20000 AND engineIndicatorLight is NOT red blinking OR commandOverride is true print "Cleared to launch!" Else print "Launch scrubbed!" */
+
+
+if (fuelLevel > 20000 && engineTemperature <= 2500) {
+    console.log("Full tank")
+} else if ()
+
+
+
